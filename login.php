5ccd132759731bef33eb3ec83b5ef70845b69a27
Bader Berro <bader.berro@lau.edu>
2023-12-01 10:10:54 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+// Hardcoded credentials for testing
+$correct_username = "admin";
+$correct_password = "beerguy";
+
+// Check if the form was submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve username and password from POST data
+    $username = isset($_POST["username"]) ? $_POST["username"] : '';
+    $password = isset($_POST["password"]) ? $_POST["password"] : '';
+
+    // Compare with hardcoded credentials
+    if ($username === $correct_username && $password === $correct_password) {
+        // Redirect to homepage
+        header('Location: homepage.html');
+        exit();
+    } else {
+        // Redirect back to the login page
+        echo "<script>alert('Wrong password!'); window.location.href='index.html';</script>";
+        exit();
+    }
+} else {
+    // Handle direct access to this script
+    echo "No data submitted!";
+}
+?>
